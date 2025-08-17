dfe0492f6249249855af2f620df6f6b4da6babc8
celerizer <33245078+celerizer@users.noreply.github.com>
2025-08-17 10:16:31 -0500

    hookup new regions to schach led

--- src/hw/schach_led.c
+++ src/hw/schach_led.c
@@ -1,22 +1,43 @@
 #include "schach_led.h"
 
+#include "../dma.h"
+
 static const char *name = "Schach LED";
 static const int type = F8_DEVICE_SCHACH_LED;
 
-/**
- * Appears to write 0x00 to first byte on boot,
- * 0xC0 on second byte when finished thinking to turn the light on
- */
+void schach_led_3800(f8_device_t *device, u16 address, f8_byte value)
+{
+  f8_schach_led_t *m_led = device->device;
+
+  F8_UNUSED(address);
+  F8_UNUSED(value);
+  m_led->state = F8_SCHACH_LED_OFF;
+}
+
+void schach_led_8000(f8_device_t *device, u16 address, f8_byte value)
+{
+  f8_schach_led_t *m_led = device->device;
+
+  F8_UNUSED(address);
+  F8_UNUSED(value);
+  m_led->state = F8_SCHACH_LED_ON;
+}
 
 void schach_led_init(f8_device_t *device)
 {
   if (device)
   {
-    /* Not sure if this is the correct size */
-    f8_generic_init(device, 256);
+    f8_schach_led_t *m_led = pf_dma_alloc(sizeof(f8_schach_led_t), TRUE);
+
+    device->device = m_led;
     device->name = name;
     device->type = type;
     device->flags = F8_DATA_WRITABLE;
+
+    device->mappings[0].length = 1;
+    device->mappings[0].func_out = schach_led_3800;
+    device->mappings[1].length = 1;
+    device->mappings[1].func_out = schach_led_8000;
 
     device->init = schach_led_init;
   }
--- src/hw/schach_led.h
+++ src/hw/schach_led.h
@@ -3,6 +3,19 @@
 
 #include "f8_device.h"
 
+typedef enum
+{
+  F8_SCACH_LED_INVALID = 0,
+
+  F8_SCHACH_LED_OFF,
+  F8_SCHACH_LED_ON
+} f8_schach_led_state;
+
+typedef struct
+{
+  f8_schach_led_state state;
+} f8_schach_led_t;
+
 void schach_led_init(f8_device_t *device);
 
 #endif
